4aa01abafd66ffa3ef3a9d848073444712c56753
xymoget <j.rasanov@gmail.com>
2025-10-05 01:44:42 +0300

    a bit better prompt

--- api/backend/api/utils/prompts.py
+++ api/backend/api/utils/prompts.py
@@ -4,6 +4,7 @@ def recipes_prompt(preferences, products):
 """
 
     return f"""Give me recipes of 4 pretty popular dishes i can make with these products{':'+', '.join(products) if products else ''} (AND salt, oil, water). Use only products I have, no other products. 
+Make sure you give exactly all products (if image is provided), check all text on packages (it can be in any language)
 Preferences: {preferences}. Use them strictly when generating dishes.
 Every recipe should be in this format:
 Dish name
